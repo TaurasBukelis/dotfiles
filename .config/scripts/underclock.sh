8786c7e299207fd4b53fbe9747522082f0b41381
Tauras Bukelis <taurasbukelis@gmail.com>
2018-04-18 13:17:55 +0300

    Added echo's

--- .config/scripts/underclock.sh
+++ .config/scripts/underclock.sh
@@ -4,10 +4,12 @@ name="Intel PState"
 turbo=$(cat /sys/devices/system/cpu/intel_pstate/no_turbo)
 if [ $turbo == 0 ]
 then
+    echo Underclocking
     echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo > /dev/null
     echo 60 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct > /dev/null
     notify-send "$name" 'Underclocked successfully'
 else
+    echo Overclocking
     echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo > /dev/null
     echo 100 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct > /dev/null
     notify-send "$name" 'Overclocked successfully'
