bc0879fe4238b6b77f33fe4775868900e693246c
Tauras Bukelis <taurasbukelis@gmail.com>
2018-04-27 18:15:32 +0300

    Cleaning up

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,9 +9,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar
 # polybar example &
 if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    for m in $(xrandr -q | grep -E ' connected [0-9]| connected primary [0-9]' | cut -d" " -f1); do
         MONITOR=$m polybar --reload example &
     done
 else
     polybar -reload example &
 fi
+
+# xrandr -q | grep -Eq "$answer connected \(|$answer connected primary \("
